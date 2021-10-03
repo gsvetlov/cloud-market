75f3c37c74dbb0e0b34eb52d129e238d20a09405
SvGA <gsvetlov@gmail.com>
2021-10-04 00:42:29 +0300

    контейнеризация eureka-service через jib тесты ProductServiceImpl

--- product-service-commons/src/main/java/ru/svetlov/cloud/market/services/product/common/dto/ProductDto.java
+++ product-service-commons/src/main/java/ru/svetlov/cloud/market/services/product/common/dto/ProductDto.java
@@ -1,9 +1,6 @@
 package ru.svetlov.cloud.market.services.product.common.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -11,6 +8,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 public class ProductDto {
     private Long id;
     private String title;
